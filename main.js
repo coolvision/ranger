1e03a2fda9ac0f97e1e75fcf922f7f7eb903498f
sergii <sk2009@gmail.com>
2023-02-19 22:42:49 +0200

    more joints

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ let boxes = [];
 // let gripper;
 // let g1, g2, g3;
 
-let arm = {};
+let robot = {};
 let parts = [];
 
 // const matrix = new THREE.Matrix4();
@@ -38,24 +38,41 @@ let parts = [];
 
 await init();
 
-function addBox(world, width, height, depth, color) {
+function addBox(world, scene, width, height, depth, x=0, y=0, z=0, color=0x333333) {
 
-    let rigid_body = world.createRigidBody(RAPIER.RigidBodyDesc.dynamic());
+RAPIER.RigidBodyDesc.dynamic()
+
+    let body_desc = RAPIER.RigidBodyDesc.dynamic().setTranslation(x, y, z);
+    let rigid_body = world.createRigidBody(body_desc);
+
     let collider = RAPIER.ColliderDesc.cuboid(width/2, height/2, depth/2);
     world.createCollider(collider, rigid_body);
 
     let geometry = new THREE.BoxGeometry(width, height, depth);
     let mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({color: color}));
 
+    scene.add(mesh);
+
     return {
         r: rigid_body,
-        m: mesh
+        m: mesh,
+        i: mesh.geometry.parameters
     }
+}
+
+function fixedJoint(r1, r2, x1=0, y1=0, z1=0, x2=0, y2=0, z2=0) {
+    return world.createImpulseJoint(RAPIER.JointData.fixed(
+        {x: x1, y: y1, z: z1}, {w: 1.0, x: 0.0, y: 0.0, z: 0.0},
+        {x: x2, y: y2, z: z2}, {w: 1.0, x: 0.0, y: 0.0, z: 0.0}), r1, r2, true);
+}
+function sphericalJoint(r1, r2, x1, y1, z1, x2=0, y2=0, z2=0) {
+    return world.createImpulseJoint(RAPIER.JointData.spherical(
+        {x: x1, y: y1, z: z1}, {x: x2, y: y2, z: z2}), r1, r2, true);
 }
 
 async function init() {
 
-    container = document.getElementById( 'app' );
+    container = document.getElementById('app');
 
     renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -67,7 +84,7 @@ async function init() {
     scene.background = new THREE.Color(0xf0f0f0);
 
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 100);
-    camera.position.set(0, 5, 5);
+    camera.position.set(2.5, 2.5, 2.5);
     scene.add(camera);
 
 //==============================================================================
@@ -76,20 +93,107 @@ async function init() {
     world = new RAPIER.World(gravity);
     eventQueue = new RAPIER.EventQueue(true);
 
+    // Create the ground
+    let groundColliderDesc = RAPIER.ColliderDesc.cuboid(10.0, 1, 10.0)
+        .setTranslation(0, -1, 0)
+    world.createCollider(groundColliderDesc);
 
-    arm.shoulder = addBox(world, 0.3, 0.3, 5, 0x333333);
-    scene.add(arm.shoulder.m)
+    let base_w = 0.4;
+    let base_h = 0.15;
+    let mast_w = 0.075;
+    let mast_h = 1.5;
+    let arm_w = 0.05;
+    let arm_base_w = arm_w*2;
+    let arm_base_h = arm_w*3;
+    let shoulder_l = 0.4;
+    let elbow_l = 0.1;
+    let forearm_l = 0.4;
+    let wrist_l = 0.1;
 
-    arm.forearm = addBox(world, 0.4, 0.4, 5, 0x333333);
-    scene.add(arm.forearm.m)
+    robot.base = addBox(world, scene, base_w, base_h, base_w, 0, base_h/2, 0);
+    robot.mast = addBox(world, scene, mast_w, mast_h, mast_w, 0, mast_h/2+base_h, 0);
 
-    let params = RAPIER.JointData.revolute(
-        {x: 0.0, y: 0.0, z: 2.75},
-        {x: 0.0, y: 0.0, z: -2.75},
-        {x: 1.0, y: 0.0, z: 0.0});
-    let joint = world.createImpulseJoint(params, arm.shoulder.r, arm.forearm.r, true);
+    robot.arm_base = addBox(world, scene, arm_base_w, arm_base_h, arm_base_w);
 
-    parts.push(arm.shoulder, arm.forearm);
+    robot.shoulder = addBox(world, scene, arm_w, arm_w, shoulder_l);
+
+    robot.elbow = addBox(world, scene, arm_w, arm_w, elbow_l);
+
+    robot.forearm = addBox(world, scene, arm_w, arm_w, forearm_l);
+
+    robot.wrist = addBox(world, scene, arm_w, arm_w, wrist_l);
+
+    robot.g1 = addBox(world, scene, 0.01, arm_w, 0.1);
+    robot.g2 = addBox(world, scene, 0.01, arm_w, 0.1);
+    robot.g3 = addBox(world, scene, 0.12, arm_w, 0.02);
+
+    parts.push(robot.base, robot.mast, robot.arm_base);
+    parts.push(robot.shoulder, robot.elbow, robot.forearm);
+    parts.push(robot.wrist, robot.g1, robot.g2, robot.g3);
+
+    robot.base.r.setAdditionalMass(100000);
+    robot.mast.r.setAdditionalMass(100000);
+    robot.arm_base.r.setAdditionalMass(1000);
+    robot.shoulder.r.setAdditionalMass(1000);
+    robot.elbow.r.setAdditionalMass(100);
+    robot.forearm.r.setAdditionalMass(100);
+    robot.wrist.r.setAdditionalMass(100);
+    robot.g3.r.setAdditionalMass(1);
+
+    let j0 = fixedJoint(robot.base.r, robot.mast.r, 0, base_h/2, 0, 0, -mast_h/2, 0);
+    let j1 = fixedJoint(robot.mast.r, robot.arm_base.r).setContactsEnabled(false);
+
+    let j2 = fixedJoint(robot.arm_base.r, robot.shoulder.r,
+        0, 0, arm_base_w/2+0.01, 0, 0, -shoulder_l/2).setContactsEnabled(false);
+
+    let j3 = fixedJoint(robot.shoulder.r, robot.elbow.r,
+        0, 0, shoulder_l/2+0.01, 0, 0, -elbow_l/2).setContactsEnabled(false);
+
+    // let j4 = fixedJoint(robot.elbow.r, robot.forearm.r,
+    //     0, 0, elbow_l, 0, 0, -forearm_l).setContactsEnabled(false);
+
+
+    // let j4 = fixedJoint(robot.elbow.r, robot.forearm.r, 0, 0, 0.21, 0, 0, -0.2).setContactsEnabled(false);
+    //
+    // let j5 = fixedJoint(robot.forearm.r, robot.wrist.r, 0, 0, 0.21, 0, 0, -0.05).setContactsEnabled(false);
+    //
+    // let j6 = fixedJoint(robot.wrist.r, robot.g3.r,
+    //     0, 0, robot.wrist.i.depth/2, 0, 0, -robot.g3.i.depth/2).setContactsEnabled(false);
+    // let j7 = fixedJoint(robot.g3.r, robot.g1.r,
+    //     0, 0, robot.g3.i.depth/2, 0.035, 0, -robot.g1.i.depth/2).setContactsEnabled(false);
+    // let j8 = fixedJoint(robot.g3.r, robot.g2.r,
+    //     0, 0, robot.g3.i.depth/2, -0.035, 0, -robot.g2.i.depth/2).setContactsEnabled(false);
+
+    // for (let i = 0; i < 1000; i++) {
+    //     world.step(eventQueue);
+    // }
+    // for (let i in parts) {
+    //     parts[i].r.setAdditionalMass(0);
+    // }
+
+
+
+
+    let x = {x: 1.0, y: 0.0, z: 0.0};
+    let y = {x: 0.0, y: 1.0, z: 0.0};
+    let z = {x: 0.0, y: 0.0, z: 1.0};
+
+    // let j1 = world.createImpulseJoint(RAPIER.JointData.revolute(
+    //     {x: 0.0, y: 0, z: 0}, {x: 0.0, y: 0.0, z: -0.3}, x),
+    //     robot.mast.r, robot.shoulder.r, true);
+    //
+    // let j2 = world.createImpulseJoint(RAPIER.JointData.revolute(
+    //     {x: 0.0, y: 0, z: 0.25}, {x: 0.0, y: 0.0, z: -0.25}, x),
+    //     robot.shoulder.r, robot.forearm.r, true);
+    //
+    // let j3 = world.createImpulseJoint(RAPIER.JointData.revolute(
+    //     {x: 0.0, y: 0, z: 0.25}, {x: 0.0, y: 0.0, z: -0.1}, x),
+    //     robot.forearm.r, robot.wrist.r, true);
+
+
+
+
+
 
 
     // let x = { x: 1.0, y: 0.0, z: 0.0 };
@@ -97,7 +201,7 @@ async function init() {
 
     // joint.configureMotorVelocity(5.0, 5);
 
-    // console.log("joint", joint)
+    console.log(robot.base, robot.mast)
 
     // (joint as RAPIER.RevoluteImpulseJoint).configureMotorVelocity(1.0, 0.5);
 
@@ -160,33 +264,30 @@ async function init() {
 
 
 
-    // Create the ground
-    let groundColliderDesc = RAPIER.ColliderDesc.cuboid(10.0, 1, 10.0)
-        .setTranslation(0, -1, 0)
-    world.createCollider(groundColliderDesc);
+
 
     let size = 0.5
-    const geometryBox = new THREE.BoxGeometry(size, size, size);
-    for (let i = 0; i < 5; i++) {
-        let material = new THREE.MeshLambertMaterial();
-        let box = new THREE.Mesh(geometryBox, material);
-
-        box.position.set(Math.random() - 0.5, Math.random() * 2, Math.random() - 0.5);
-        box.position.multiplyScalar(5);
-        box.material.color.setHex(0xffffff * Math.random());
-
-        let rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
-                .setTranslation(box.position.x, box.position.y, box.position.z)
-                // .setCcdEnabled(true);
-        let rigidBody = world.createRigidBody(rigidBodyDesc);
-        let colliderDesc = RAPIER.ColliderDesc.cuboid(size/2, size/2, size/2);
-        let collider = world.createCollider(colliderDesc, rigidBody);
-
-        box.rigidBody = rigidBody;
-
-        scene.add(box);
-        boxes.push(box);
-    }
+    // const geometryBox = new THREE.BoxGeometry(size, size, size);
+    // for (let i = 0; i < 5; i++) {
+    //     let material = new THREE.MeshLambertMaterial();
+    //     let box = new THREE.Mesh(geometryBox, material);
+    //
+    //     box.position.set(Math.random() - 0.5, Math.random() * 2, Math.random() - 0.5);
+    //     box.position.multiplyScalar(5);
+    //     box.material.color.setHex(0xffffff * Math.random());
+    //
+    //     let rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
+    //             .setTranslation(box.position.x, box.position.y, box.position.z)
+    //             // .setCcdEnabled(true);
+    //     let rigidBody = world.createRigidBody(rigidBodyDesc);
+    //     let colliderDesc = RAPIER.ColliderDesc.cuboid(size/2, size/2, size/2);
+    //     let collider = world.createCollider(colliderDesc, rigidBody);
+    //
+    //     box.rigidBody = rigidBody;
+    //
+    //     scene.add(box);
+    //     boxes.push(box);
+    // }
 
 
     // let gripperGeometry1 = new THREE.BoxGeometry(0.1, 0.5, 1);
@@ -276,9 +377,9 @@ function render() {
 
     world.step(eventQueue);
 
-    eventQueue.drainCollisionEvents((handle1, handle2, started) => {
-        console.log("collision", handle1, handle2, started, g1.body, g2.body);
-    });
+    // eventQueue.drainCollisionEvents((handle1, handle2, started) => {
+    //     console.log("collision", handle1, handle2, started, g1.body, g2.body);
+    // });
 
     for (let i = 0; i < boxes.length; i++) {
         let p = boxes[i].rigidBody.translation();
@@ -340,9 +441,9 @@ window.addEventListener( 'keydown', function ( event ) {
             break;
 
         case 16: // Shift
-            transform_ctrl.setTranslationSnap( 100 );
-            transform_ctrl.setRotationSnap( THREE.MathUtils.degToRad( 15 ) );
-            transform_ctrl.setScaleSnap( 0.25 );
+            // transform_ctrl.setTranslationSnap( 100 );
+            // transform_ctrl.setRotationSnap( THREE.MathUtils.degToRad( 15 ) );
+            // transform_ctrl.setScaleSnap( 0.25 );
             break;
 
         case 87: // W
