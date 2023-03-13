1fded19332cc235bafeb597f826d1505f6fb55c2
sergii <sk2009@gmail.com>
2023-03-13 15:32:33 +0200

    joints

--- main.js
+++ main.js
@@ -112,20 +112,19 @@ function addBody(type, shape, world, scene, g, m, f, d, width, height, depth, x=
 }
 
 function fixedJoint(r1, r2, x1=0, y1=0, z1=0, x2=0, y2=0, z2=0) {
-    return world.createImpulseJoint(RAPIER.JointData.fixed(
-        {x: x1, y: y1, z: z1}, {w: 1.0, x: 0.0, y: 0.0, z: 0.0},
-        {x: x2, y: y2, z: z2}, {w: 1.0, x: 0.0, y: 0.0, z: 0.0}), r1, r2, true);
+    let j = world.createImpulseJoint(RAPIER.JointData.fixed(
+            {x: x1, y: y1, z: z1}, {w: 1.0, x: 0.0, y: 0.0, z: 0.0},
+            {x: x2, y: y2, z: z2}, {w: 1.0, x: 0.0, y: 0.0, z: 0.0}), r1.r, r2.r, true);
+    j.r1 = r1;
+    j.r2 = r2;
+    return j;
 }
 function revoluteJoint(r1, r2, axis, x1=0, y1=0, z1=0, x2=0, y2=0, z2=0) {
-    return world.createImpulseJoint(RAPIER.JointData.revolute(
-        {x: x1, y: y1, z: z1}, {x: x2, y: y2, z: z2}, axis), r1, r2, true);
-}
-function prismaticJoint(r1, r2, axis, l1, l2, x1=0, y1=0, z1=0, x2=0, y2=0, z2=0) {
-    let params = RAPIER.JointData.prismatic(
-        {x: x1, y: y1, z: z1}, {x: x2, y: y2, z: z2}, axis)
-    params.limitsEnabled = true;
-    params.limits = [l1, l2];
-    return world.createImpulseJoint(params, r1, r2, true);
+    let j = world.createImpulseJoint(RAPIER.JointData.revolute(
+        {x: x1, y: y1, z: z1}, {x: x2, y: y2, z: z2}, axis), r1.r, r2.r, true);
+    j.r1 = r1;
+    j.r2 = r2;
+    return j;
 }
 
 async function init() {
@@ -209,14 +208,14 @@ async function init() {
     let y = {x: 0.0, y: 1.0, z: 0.0};
     let z = {x: 0.0, y: 0.0, z: 1.0};
 
-    let j0 = fixedJoint(r.base.r, r.mast.r, 0, r.base.h/2, 0, 0, -r.mast.h/2, 0);
-    let ji = fixedJoint(r.base.r, r.indicator.r, 0, r.base.h/2, r.base.w/2);
-    let j1 = revoluteJoint(r.mast.r, r.arm_base.r, y, 0, 0, 0, -arm_w*0.75, 0, 0);
-    let j2 = revoluteJoint(r.arm_base.r, r.shoulder.r, x, r.arm_base.w/2, 0, 0, -arm_w/2, 0, -r.shoulder.d/2);
-    let j3 = revoluteJoint(r.shoulder.r, r.elbow.r, x,  -arm_w/2, 0, r.shoulder.d/2-arm_w/2,  arm_w/2, 0, -r.elbow.d/2);
-    let j4 = revoluteJoint(r.elbow.r, r.forearm.r, z, 0, 0, r.elbow.d/2, 0, 0, -r.forearm.d/2);
-    let j5 = revoluteJoint(r.forearm.r, r.wrist.r, x, arm_w/2, 0, r.forearm.d/2-arm_w/2, -arm_w/2, 0, -r.wrist.d/2);
-    let j6 = revoluteJoint(r.wrist.r, r.g3.r, z, 0, 0, r.wrist.d/2, 0, 0, -r.g3.d/2);
+    let j0 = fixedJoint(r.base, r.mast, 0, r.base.h/2, 0, 0, -r.mast.h/2, 0);
+    let ji = fixedJoint(r.base, r.indicator, 0, r.base.h/2, r.base.w/2);
+    let j1 = revoluteJoint(r.mast, r.arm_base, y, 0, 0, 0, -arm_w*0.75, 0, 0);
+    let j2 = revoluteJoint(r.arm_base, r.shoulder, x, r.arm_base.w/2, 0, 0, -arm_w/2, 0, -r.shoulder.d/2);
+    let j3 = revoluteJoint(r.shoulder, r.elbow, x,  -arm_w/2, 0, r.shoulder.d/2-arm_w/2,  arm_w/2, 0, -r.elbow.d/2);
+    let j4 = revoluteJoint(r.elbow, r.forearm, z, 0, 0, r.elbow.d/2, 0, 0, -r.forearm.d/2);
+    let j5 = revoluteJoint(r.forearm, r.wrist, x, arm_w/2, 0, r.forearm.d/2-arm_w/2, -arm_w/2, 0, -r.wrist.d/2);
+    let j6 = revoluteJoint(r.wrist, r.g3, z, 0, 0, r.wrist.d/2, 0, 0, -r.g3.d/2);
 
     joints.push(j0, j1, j2, j3, j4, j5, j6);
     for (let j in joints) {
@@ -334,15 +333,45 @@ function render() {
     }
 
 //==============================================================================
+
     stop_arm_motion = false;
     for (let i in parts) {
         world.contactsWith(parts[i].c, (c2) => {
             if (!c2.is_robot && !c2.ignore_controller) {
-                // console.log("stop_arm_motion", i, c2);
-                stop_arm_motion = true;
+                if (parts[i].c.contactCollider(c2, 0.001)) {
+                    console.log("stop_arm_motion", i, c2);
+                    stop_arm_motion = true;
+                }
             }
         });
     }
+
+    for (let j in joints) {
+        let a1 = joints[j].anchor1();
+        let a2 = joints[j].anchor2();
+
+        let p1 = new THREE.Vector3();
+        p1.set(a1.x, a1.y, a1.z);
+        let p2 = new THREE.Vector3();
+        p2.set(a2.x, a2.y, a2.z);
+
+        // console.log("joint L", j, p1, p2);
+
+        let w1 = p1.clone();
+        let w2 = p2.clone();
+
+        joints[j].r1.m.localToWorld(w1);
+        joints[j].r2.m.localToWorld(w2);
+
+        if (Math.abs(w1.x-w2.x) > 0.01 ||
+            Math.abs(w1.y-w2.y) > 0.01 ||
+            Math.abs(w1.z-w2.z) > 0.01) {
+            console.log("joint W", j, "w1", w1.x.toFixed(3), w1.y.toFixed(3), w1.z.toFixed(3),
+                "w2",  w2.x.toFixed(3), w2.y.toFixed(3), w2.z.toFixed(3));
+        }
+    }
+
+
 
     if (!stop_arm_motion) {
 
@@ -356,13 +385,13 @@ function render() {
             parts[i].lgt = p1;
             parts[i].lgr = q1;
 
-            if (i >= 5 && i <= 8) console.log("save", i, parts[i].r.translation().x,
-                                    parts[i].r.translation().y,
-                                    parts[i].r.translation().z);
-            if (i >= 5 && i <= 8) console.log("save q", i, parts[i].r.rotation().x,
-                                    parts[i].r.rotation().y,
-                                    parts[i].r.rotation().z,
-                                    parts[i].r.rotation().w);
+            // if (i >= 5 && i <= 8) console.log("save", i, parts[i].r.translation().x,
+            //                         parts[i].r.translation().y,
+            //                         parts[i].r.translation().z);
+            // if (i >= 5 && i <= 8) console.log("save q", i, parts[i].r.rotation().x,
+            //                         parts[i].r.rotation().y,
+            //                         parts[i].r.rotation().z,
+            //                         parts[i].r.rotation().w);
 
             if (parts[i].t == "dynamic") parts[i].r.setBodyType(0);
         }
@@ -375,33 +404,31 @@ function render() {
         //                         r.g3.r.translation().y,
         //                         r.g3.r.translation().z);
 
-
-
     } else {
 
-        console.log("stop_arm_motion", r.g3.r.translation())
-        console.log("******************************************************************************");
-        console.log("******************************************************************************");
-        console.log("******************************************************************************");
+        // console.log("stop_arm_motion", r.g3.r.translation())
+        // console.log("******************************************************************************");
+        // console.log("******************************************************************************");
+        // console.log("******************************************************************************");
 
         print_frames_i = 5;
 
-        for (let i in parts) {
-            if (i >= 5 && i <= 8) console.log("before", i, parts[i].r.translation().x,
-                                    parts[i].r.translation().y,
-                                    parts[i].r.translation().z, parts[i].r.linvel(), parts[i].r.angvel());
-            if (i >= 5 && i <= 8) console.log("before q", i, parts[i].r.rotation().x,
-                                    parts[i].r.rotation().y,
-                                    parts[i].r.rotation().z,
-                                    parts[i].r.rotation().w);
-        }
+        // for (let i in parts) {
+        //     if (i >= 5 && i <= 8) console.log("before", i, parts[i].r.translation().x,
+        //                             parts[i].r.translation().y,
+        //                             parts[i].r.translation().z, parts[i].r.linvel(), parts[i].r.angvel());
+        //     if (i >= 5 && i <= 8) console.log("before q", i, parts[i].r.rotation().x,
+        //                             parts[i].r.rotation().y,
+        //                             parts[i].r.rotation().z,
+        //                             parts[i].r.rotation().w);
+        // }
 
         for (let i in parts) {
             let p = parts[i].lgt.clone();
             let q = parts[i].lgr.clone();
 
-            if (i >= 5 && i <= 8) console.log("restore", i, p.x, p.y, p.z);
-            if (i >= 5 && i <= 8) console.log("restore q ", i, q.x, q.y, q.z, q.w);
+            // if (i >= 5 && i <= 8) console.log("restore", i, p.x, p.y, p.z);
+            // if (i >= 5 && i <= 8) console.log("restore q ", i, q.x, q.y, q.z, q.w);
 
             parts[i].r.setBodyType(2);
             parts[i].r.setTranslation({x: p.x, y: p.y, z: p.z}, true);
@@ -415,7 +442,7 @@ function render() {
             parts[i].r.resetTorques(true);
             parts[i].r.recomputeMassPropertiesFromColliders();
 
-            if (i >= 5 && i <= 8) console.log("restore v", i, parts[i].r.linvel(), parts[i].r.angvel());
+            // if (i >= 5 && i <= 8) console.log("restore v", i, parts[i].r.linvel(), parts[i].r.angvel());
 
         }
         // let p2 = r.g3.lgt.clone();
@@ -423,22 +450,22 @@ function render() {
         // pointer_target.position.set(p2.x, p2.y, p2.z);
 
 
-        for (let i in parts) {
-            if (i >= 5 && i <= 8) console.log("after", i, parts[i].r.translation().x,
-                                    parts[i].r.translation().y,
-                                    parts[i].r.translation().z, parts[i].r.linvel(), parts[i].r.angvel());
-            if (i >= 5 && i <= 8) console.log("after q", i, parts[i].r.rotation().x,
-                                    parts[i].r.rotation().y,
-                                    parts[i].r.rotation().z,
-                                    parts[i].r.rotation().w);
-        }
-
-        console.log("stop frame_i", frame_i, r.g3.r.translation().x,
-                                r.g3.r.translation().y,
-                                r.g3.r.translation().z);
+        // for (let i in parts) {
+        //     if (i >= 5 && i <= 8) console.log("after", i, parts[i].r.translation().x,
+        //                             parts[i].r.translation().y,
+        //                             parts[i].r.translation().z, parts[i].r.linvel(), parts[i].r.angvel());
+        //     if (i >= 5 && i <= 8) console.log("after q", i, parts[i].r.rotation().x,
+        //                             parts[i].r.rotation().y,
+        //                             parts[i].r.rotation().z,
+        //                             parts[i].r.rotation().w);
+        // }
+        //
+        // console.log("stop frame_i", frame_i, r.g3.r.translation().x,
+        //                         r.g3.r.translation().y,
+        //                         r.g3.r.translation().z);
     }
 
-    console.log("frame_i", frame_i);
+    // console.log("frame_i", frame_i);
 
     // let p = new THREE.Vector3();
     // pointer_target.getWorldPosition(p);
@@ -510,15 +537,15 @@ function render() {
 
     world.step(eventQueue);
 
-    for (let i in parts) {
-        if (i >= 5 && i <= 8) console.log("after step ", i, parts[i].r.translation().x,
-                                parts[i].r.translation().y,
-                                parts[i].r.translation().z, parts[i].r.linvel(), parts[i].r.angvel());
-        if (i >= 5 && i <= 8) console.log("after step q", i, parts[i].r.rotation().x,
-                                parts[i].r.rotation().y,
-                                parts[i].r.rotation().z,
-                                parts[i].r.rotation().w);
-    }
+    // for (let i in parts) {
+    //     if (i >= 5 && i <= 8) console.log("after step ", i, parts[i].r.translation().x,
+    //                             parts[i].r.translation().y,
+    //                             parts[i].r.translation().z, parts[i].r.linvel(), parts[i].r.angvel());
+    //     if (i >= 5 && i <= 8) console.log("after step q", i, parts[i].r.rotation().x,
+    //                             parts[i].r.rotation().y,
+    //                             parts[i].r.rotation().z,
+    //                             parts[i].r.rotation().w);
+    // }
 
 
 //==============================================================================
